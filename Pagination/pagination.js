a0f3ad740dc3ebc8cfb0bf29ca4b2a9f81db68e8
Erickson Sernero <140161290+noskcire101@users.noreply.github.com>
2025-09-26 22:37:36 +0800

    Update pagination.js

--- Pagination/pagination.js
+++ Pagination/pagination.js
@@ -1,55 +1,56 @@
 <script async type="module" src="https://cdn.jsdelivr.net/npm/@finsweet/attributes@2/attributes.js" fs-list></script>
 <script>
-const wrapper = document.querySelector('.w-dyn-list');
-if (wrapper) {
-  wrapper.classList.add('fs-cms_wrapper');
+  const wrappers = document.querySelectorAll('.w-dyn-list');
+  // We use forEach so it will still work even you have multiple collection list all together in one page
+  wrappers.forEach((wrapper) => {
+    wrapper.classList.add('fs-cms_wrapper');
 
-  const listItems = wrapper.querySelector('.w-dyn-items');
-  if (listItems) {
-    listItems.classList.add('fs-card_list');
-    listItems.setAttribute('fs-list-element', 'list');
-    listItems.setAttribute('fs-list-load', 'pagination');
-    listItems.setAttribute('fs-list-pagesiblings', '2');
-    listItems.setAttribute('fs-list-pageboundary', '2');
-  }
-
-  const paginationWrapper = wrapper.querySelector('.w-pagination-wrapper');
-  if (paginationWrapper) {
-    paginationWrapper.classList.add('fs-list_pagination');
-
-    const prev = paginationWrapper.querySelector('.w-pagination-previous');
-    if (prev) {
-      prev.classList.add('fs-list_pagination_prev', 'is-list-pagination-disabled');
+    const listItems = wrapper.querySelector('.w-dyn-items');
+    if (listItems) {
+      listItems.classList.add('fs-card_list');
+      listItems.setAttribute('fs-list-element', 'list');
+      listItems.setAttribute('fs-list-load', 'pagination');
+      listItems.setAttribute('fs-list-pagesiblings', '2');
+      listItems.setAttribute('fs-list-pageboundary', '2');
     }
 
-    const next = paginationWrapper.querySelector('.w-pagination-next');
-    if (next) {
-      next.classList.add('fs-list_pagination_next', 'is-list-pagination-disabled');
+    const paginationWrapper = wrapper.querySelector('.w-pagination-wrapper');
+    if (paginationWrapper) {
+      paginationWrapper.classList.add('fs-list_pagination');
+
+      const prev = paginationWrapper.querySelector('.w-pagination-previous');
+      if (prev) {
+        prev.classList.add('fs-list_pagination_prev', 'is-list-pagination-disabled');
+      }
+
+      const next = paginationWrapper.querySelector('.w-pagination-next');
+      if (next) {
+        next.classList.add('fs-list_pagination_next', 'is-list-pagination-disabled');
+      }
+
+      const pageButtonsDiv = document.createElement('div');
+      pageButtonsDiv.classList.add('list_page-buttons');
+
+      const pageButton = document.createElement('a');
+      pageButton.classList.add('fs-list_pagination_button');
+      pageButton.setAttribute('fs-list-element', 'page-button');
+
+      const dotsDiv = document.createElement('div');
+      dotsDiv.classList.add('fs-list_pagination_dots');
+      dotsDiv.setAttribute('fs-list-element', 'page-dots');
+
+      const dotsP = document.createElement('p');
+      dotsP.textContent = '...';
+      dotsDiv.appendChild(dotsP);
+
+      pageButtonsDiv.appendChild(pageButton);
+      pageButtonsDiv.appendChild(dotsDiv);
+
+      if (next) {
+        paginationWrapper.insertBefore(pageButtonsDiv, next);
+      } else {
+        paginationWrapper.appendChild(pageButtonsDiv);
+      }
     }
-
-    const pageButtonsDiv = document.createElement('div');
-    pageButtonsDiv.classList.add('list_page-buttons');
-
-    const pageButton = document.createElement('a');
-    pageButton.classList.add('fs-list_pagination_button');
-    pageButton.setAttribute('fs-list-element', 'page-button');
-
-    const dotsDiv = document.createElement('div');
-    dotsDiv.classList.add('fs-list_pagination_dots');
-    dotsDiv.setAttribute('fs-list-element', 'page-dots');
-
-    const dotsP = document.createElement('p');
-    dotsP.textContent = '...';
-    dotsDiv.appendChild(dotsP);
-
-    pageButtonsDiv.appendChild(pageButton);
-    pageButtonsDiv.appendChild(dotsDiv);
-
-    if (next) {
-      paginationWrapper.insertBefore(pageButtonsDiv, next);
-    } else {
-      paginationWrapper.appendChild(pageButtonsDiv);
-    }
-  }
-}
+  });
 </script>
